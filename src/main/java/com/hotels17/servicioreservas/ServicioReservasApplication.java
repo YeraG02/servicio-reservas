2b38a8ed50822bdbbc7fee32a7fb06a6352427e2
Yera <yera.gonzalez.landa@zabalburu.org>
2022-03-02 12:38:42 +0100

    Initial commit

---
+++ src/main/java/com/hotels17/servicioreservas/ServicioReservasApplication.java
@@ -0,0 +1,13 @@
+package com.hotels17.servicioreservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioReservasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioReservasApplication.class, args);
+    }
+
+}
